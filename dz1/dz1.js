7b84dab93a693a328a8a214441f05fdc7de1a02c
vikjaaa <vika.korniyenko@gmail.com>
2023-11-21 15:21:51 +0200

    dz1

---
+++ dz1/dz1.js
@@ -0,0 +1,39 @@
+let s1 = "hello";
+let s2 ="owu";
+let s3 = "com";
+let s4 ="ua";
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+let n5 = 3.14;
+let n6 = 2.7;
+let n7 = 16;
+let b1 = true;
+let b2 = false;
+console.log(s1)
+console.log(s2)
+console.log(s3)
+console.log(s4)
+console.log(n1)
+console.log(n2)
+console.log(n3)
+console.log(n4)
+console.log(n5)
+console.log(n6)
+console.log(n7)
+console.log(b1)
+console.log(b2)
+
+let firstName = 'Viktoriya ';
+let lastName ='Korniyenko ';
+let middleName = 'Sergiivna ';
+person = firstName + lastName + middleName;
+console.log(person)
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
